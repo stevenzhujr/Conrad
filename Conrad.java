afb24024887b4959b9d350402e376b23f7be9c9c
Steven <user.name>
2023-10-24 00:44:06 -0700

    graphicsandpause

--- Conrad.java
+++ Conrad.java
@@ -4,12 +4,20 @@ import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.Frame; 
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*; 
 
 public class Conrad extends Frame{
+    static boolean cont = true;
     public Conrad() 
     { 
         setVisible(true); 
-        setSize(1000, 1000); 
+        setSize(1000, 1000);
+        setLayout(null);
         addWindowListener(new WindowAdapter() { 
             @Override
             public void windowClosing(WindowEvent e) 
@@ -18,15 +26,27 @@ public class Conrad extends Frame{
             } 
         }); 
     } 
-    public void paint(Graphics g, int i, int j) 
-    { 
-        g.drawRect(i, j, 100, 50); 
+    public void paint(Graphics g, int[][] grid) 
+    {
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,1000,1000);
+        g.setColor(Color.BLACK);
+        for (int i = 60; i<260; i+=20){
+            for (int j = 60; j<260; j+=20){
+                g.drawRect(i, j, 20, 20);
+                if (grid[i/20-3][j/20-3]==1){
+                    g.setColor(Color.BLACK);
+                    g.fillRect(i,j,20,20);
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int height = 10;
         int width = 10;
         int[][] grid = new int[height][width];
+        grid[0][0]=1;
         grid[5][5]=1;
         grid[4][5]=1;
         grid[5][4]=1;
@@ -34,50 +54,61 @@ public class Conrad extends Frame{
         grid[2][3]=1;
         grid[3][3]=1;
         grid[9][9]=1;
-        for (int i = 0; i<height; i++){
-            for (int j = 0; j<width; j++){
-                System.out.print(grid[i][j]);
-            }
-            System.out.println(" ");
-        }
         Conrad c = new Conrad();
-        c.paint(c.getGraphics(),100,100);
-        c.paint(c.getGraphics(), 100, 100);
-        int i = 0;
-        int j = 0;
+        boolean first = true;
+        Button b = new Button("Pause");
+        b.setBounds(60,300,100,50);
+        b.addActionListener(new ActionListener() {    
+            public void actionPerformed (ActionEvent e) {    
+                    if (cont){
+                        cont = false;
+                    } else {
+                        cont = true;
+                    }
+                    System.out.print(cont);
+                }    
+            });
+        c.add(b);
         while (true){
-            c.paint(c.getGraphics(),i,j);
-            i++;
-            j++;
+            if (cont){
+                c.paint(c.getGraphics(),grid);
+                if (!first){
+                    grid = next(grid, 10, 10);
+                } else {
+                    first = false;
+                }
+            }
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e){
                 System.out.print("fuck");
             }
-            
         }
     }
     public static int[][] next(int grid[][], int height, int width){
-        int[][] next = new int[height][width];
-        next = grid;
+        int[][] next = new int[10][10];
         for (int i = 0; i<height; i++){
             for (int j = 0; j<width; j++){
                 int alive = 0;
                 for (int x = -1; x < 2; x++){
                     for (int y = -1; y < 2; y++){
-                        if (i+x>=0 && i+x<height && j+y>=0 && j+y<width){
-                            if (grid[i+x][j+y]==1 && (x!=0 || y!=0)){
+                        if (i==4 && j==3){
+                        }
+                        if (i+x >= 0 && i+x<height && j+y >= 0 && j+y<width){
+                            if (grid[i+x][j+y]==1 && !(x==0 && y==0)){
                                 alive++;
                             }
                         }
                     }
                 }
-                if (next[i][j] == 1 && alive<2){
+                if (grid[i][j] == 1 && alive<2){
                     next[i][j]=0;
-                } else if (next[i][j] == 1 && alive>3){
+                } else if (grid[i][j] == 1 && alive>3){
                     next[i][j]=0;
-                } else if (next[i][j] == 0 && alive==3){
+                } else if (grid[i][j] == 0 && alive==3){
                     next[i][j]=1;
+                } else {
+                    next[i][j]=grid[i][j];
                 }
             }
         }
