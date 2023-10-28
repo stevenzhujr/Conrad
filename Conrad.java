ae97322322582659f1bc65adbffde41950c0926a
Steven <user.name>
2023-10-27 20:26:49 -0700

    dupeImport

--- Conrad.java
+++ Conrad.java
@@ -6,7 +6,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.Frame; 
 import java.awt.Graphics2D;
-import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.Color;
 import java.awt.Button;
